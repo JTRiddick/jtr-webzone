56185e982b4f9926833312d7fd4e2aec7022c4ef
J Taylor Riddick <riddick.taylor@gmail.com>
2017-02-25 10:35:43 -0600

    removed a lot of cruft

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
 
- $("#quincy-content").hide();
-   // jQuery methods go here...
-	 $('#quincy').click(function(){
+ $("#extended-content").hide();
 
-	 	var $buttontext = $(this);
- 	 	$buttontext.text($buttontext.text() == "I Would Like To Know Less" ? "I Would Like To Know More" : "I Would Like To Know Less");
+  $('#extend-btn').click(function(){
+ 	  var $buttontext = $(this);
+	 	$buttontext.text($buttontext.text() == "I Would Like To Know Less" ? "I Would Like To Know More" : "I Would Like To Know Less");
 
-	  	$("#quincy-content").toggle();
+  	$("#extended-content").toggle();
 
-
-
-	 });
+  });
 
 
 
-}); 
+});
